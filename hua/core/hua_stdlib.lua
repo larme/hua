ed0797222ac898d06826a816620e13dd322fcd69
Zhao Shenyang <dev@zsy.im>
2015-05-02 04:33:38 +0800

    update stdlib

--- hua/core/hua_stdlib.lua
+++ hua/core/hua_stdlib.lua
@@ -14,5 +14,12 @@ local inc = nil
 inc = function (n)
   return (n + 1)
 end
-local EXPORT = {dec = dec, inc = inc, apply = apply, first = first}
-return(EXPORT)
+local _hua_anon_var_1
+do
+  local _hua_module_1235 = {}
+  assoc(_hua_module_1235,"apply",apply)
+  assoc(_hua_module_1235,"dec",dec)
+  assoc(_hua_module_1235,"first",first)
+  assoc(_hua_module_1235,"inc",inc)
+  return _hua_module_1235
+end
