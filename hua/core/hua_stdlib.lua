cac627e6f37fe3fc85646d56ffda6918172e471d
Zhao Shenyang <dev@zsy.im>
2015-05-02 04:37:44 +0800

    update stdlib 2

--- hua/core/hua_stdlib.lua
+++ hua/core/hua_stdlib.lua
@@ -17,9 +17,9 @@ end
 local _hua_anon_var_1
 do
   local _hua_module_1235 = {}
-  assoc(_hua_module_1235,"apply",apply)
-  assoc(_hua_module_1235,"dec",dec)
-  assoc(_hua_module_1235,"first",first)
-  assoc(_hua_module_1235,"inc",inc)
+  _hua_module_1235.apply = apply
+  _hua_module_1235.dec = dec
+  _hua_module_1235.first = first
+  _hua_module_1235.inc = inc
   return _hua_module_1235
 end
